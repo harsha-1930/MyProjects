8e53fe58d03c9eb1884edd9e49c0e618ce181d01
harsha-1930 <harshamuddana4@gmail.com>
2026-06-04 18:22:13 +0530

    Updated Schema

--- E-Commerce-Logistics-SQL-main/schema.sql
+++ E-Commerce-Logistics-SQL-main/schema.sql
@@ -1,13 +1,14 @@
--- Drop old tables if they exist
-DROP TABLE IF EXISTS customers;
+-- Drop old tables if they exists
+
+DROP TABLE IF EXISTS deliverylogs;
+DROP TABLE IF EXISTS shipments;
 DROP TABLE IF EXISTS orders;
 DROP TABLE IF EXISTS partners;
-DROP TABLE IF EXISTS shipments;
-DROP TABLE IF EXISTS deliverylogs;
+DROP TABLE IF EXISTS customers;
 
 -- Customers table
 CREATE TABLE customers (
-    customer_id AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     address TEXT
@@ -15,36 +16,41 @@ CREATE TABLE customers (
 
 -- Orders table
 CREATE TABLE orders (
-    order_id AUTO_INCREMENT PRIMARY KEY,
-    customer_id INT REFERENCES customers(customer_id),
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
     product VARCHAR(100) NOT NULL,
     quantity INT NOT NULL,
-    order_date DATE DEFAULT CURRENT_DATE - INTERVAL 5 DAY,
-    status VARCHAR(20) DEFAULT 'pending'
+    order_date DATE DEFAULT CURRENT_DATE,
+    status VARCHAR(20) DEFAULT 'pending',
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
 );
 
 -- Partners table
 CREATE TABLE partners (
-    partner_id AUTO_INCREMENT PRIMARY KEY,
+    partner_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     contact_email VARCHAR(100)
 );
 
 -- Shipments table
 CREATE TABLE shipments (
-    shipment_id AUTO_INCREMENT PRIMARY KEY,
-    partner_id INT REFERENCES partners(partner_id),
-    order_id INT REFERENCES orders(order_id),
+    shipment_id INT AUTO_INCREMENT PRIMARY KEY,
+    partner_id INT,
+    order_id INT,
     promised_date DATE NOT NULL,
     actual_delivery_date DATE,
     destination_city VARCHAR(100),
-    status VARCHAR(20) DEFAULT 'in_transit'
+    status VARCHAR(20) DEFAULT 'in_transit',
+    FOREIGN KEY (partner_id) REFERENCES partners(partner_id),
+    FOREIGN KEY (order_id) REFERENCES orders(order_id)
 );
 
 -- DeliveryLogs table
 CREATE TABLE deliverylogs (
-    log_id AUTO_INCREMENT PRIMARY KEY,
-    shipment_id INT REFERENCES shipments(shipment_id),
+    log_id INT AUTO_INCREMENT PRIMARY KEY,
+    shipment_id INT,
     log_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    event VARCHAR(50) CHECK (event IN ('PickedUp','InTransit','Delivered','Returned'))
+    event VARCHAR(50),
+    FOREIGN KEY (shipment_id) REFERENCES shipments(shipment_id),
+    CHECK (event IN ('PickedUp','InTransit','Delivered','Returned'))
 );
